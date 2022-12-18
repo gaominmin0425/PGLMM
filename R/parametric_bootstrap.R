3a05222243728b703c4bedb5285dccb0db5db5ee
Ruimin Gao <ruimingao0425@gmail.com>
2022-12-17 21:30:39 -0500

    update bootstrapping

--- R/parametric_bootstrap.R
+++ R/parametric_bootstrap.R
@@ -1,3 +1,5 @@
+library(broom.mixed)
+library(tidyverse)
 para_bootstrap <- function(B, Dataset){
   ## we first re-sample the random effect Z_i
   n = nrow(Dataset)
@@ -10,17 +12,14 @@ para_bootstrap <- function(B, Dataset){
   Zi_1 = rep(Zi,each = 3)
   ##we fitted the model with random effects to compute mu_i_hat
   mu_i_hat = exp(log(Dataset1$Area)+tortoise_fit$beta[1]+(tortoise_fit$beta[2]*Dataset1$prev
-                                                         +tortoise_fit$beta[3]*factor(Dataset1$Year2005)
-                                                         +tortoise_fit$beta[4]*factor(Dataset1$Year2006)
+                                                         +tortoise_fit$beta[3]*Dataset1$Year2005
+                                                         +tortoise_fit$beta[4]*Dataset1$Year2006
                                                          +Zi_1))
   ## Generate B times of Y_ij from poisson model, and then refit the model to obtain parametric bootstrapped estimates.
-  Dataset_new = Dataset1 %>% mutate(mu_i_hat = mu_i_hat) %>% crossing(B=1:B) %>% mutate(Y_hat = rpois(n, mu_i_hat))
-  Bootstrap1 = Dataset_new %>% group_by(B) %>% summarize(tidy(glmer(Y_hat~prev +offset(log(Area))+factor(year)+(1|site),
+  Dataset_new = Dataset1 %>% mutate(mu_i_hat = mu_i_hat) %>% crossing(B=1:B) %>% mutate(Y_hat = rpois(n(), mu_i_hat))
+  Bootstrap1 = Dataset_new %>% group_by(B) %>% summarise(tidy(glmer(Y_hat~prev + offset(log(Area))+factor(year)+(1|Site),
                                                                     family = poisson)), .groups = "drop")
   ## compute bootstrap standard error and confidence interval
-
-
-
-
+  Bootstrap1 %>% filter(term == "prev") %>% summarize(SD = sd(estimate),Lower95 = quantile(estimate, .025),Upper95 = quantile(estimate,.975))
 
 }
