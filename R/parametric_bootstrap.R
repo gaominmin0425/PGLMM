062d7b301d9902066d9e5cf496c1d38b4faaabf6
Ruimin Gao <ruimingao0425@gmail.com>
2022-12-17 21:00:10 -0500

    update code for bootstrap

---
+++ R/parametric_bootstrap.R
@@ -0,0 +1,26 @@
+para_bootstrap <- function(B, Dataset){
+  ## we first re-sample the random effect Z_i
+  n = nrow(Dataset)
+  i = 10
+  j = 3
+  Dataset1 = Dataset %>% mutate(Year2005 = ifelse(Dataset$year == "2005",1,0), Year2006 = ifelse(Dataset$year=="2006", 1,0))
+  tortoise_fit = run_model(Dataset1, "tortoise")
+  Sigmasq = tortoise_fit$sigmasq
+  Zi = rnorm(i, mean =0, sd = sqrt(Sigmasq))
+  Zi_1 = rep(Zi,each = 3)
+  ##we fitted the model with random effects to compute mu_i_hat
+  mu_i_hat = exp(log(Dataset1$Area)+tortoise_fit$beta[1]+(tortoise_fit$beta[2]*Dataset1$prev
+                                                         +tortoise_fit$beta[3]*factor(Dataset1$Year2005)
+                                                         +tortoise_fit$beta[4]*factor(Dataset1$Year2006)
+                                                         +Zi_1))
+  ## Generate B times of Y_ij from poisson model, and then refit the model to obtain parametric bootstrapped estimates.
+  Dataset_new = Dataset1 %>% mutate(mu_i_hat = mu_i_hat) %>% crossing(B=1:B) %>% mutate(Y_hat = rpois(n, mu_i_hat))
+  Bootstrap1 = Dataset_new %>% group_by(B) %>% summarize(tidy(glmer(Y_hat~prev +offset(log(Area))+factor(year)+(1|site),
+                                                                    family = poisson)), .groups = "drop")
+  ## compute bootstrap standard error and confidence interval
+
+
+
+
+
+}
